94296f7d84ca3a4a42101605fd1c575441ccf27a
line0 <line0@mail.lv>
2015-01-04 15:16:10 +0100

    remove extraneous parenthesis

--- l0/ASSTags.lua
+++ l0/ASSTags.lua
@@ -74,7 +74,7 @@ function ASSBase:checkRange(min,max,...)
     local vals = table.pack(...)
     for i=1,vals.n do
         assertEx(vals[i] >= min and vals[i] <= max, "%s must be in range %d-%d, got %d.",
-                 self.typeName, min, max, vals[i]))
+                 self.typeName, min, max, vals[i])
     end
 end
 
